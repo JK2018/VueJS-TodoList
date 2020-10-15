25ac4b8d2e0ab264960062acd8dfe71de3033f56
JK2018 <khalifa_jonathan@hotmail.com>
2020-10-15 21:46:36 +0200

    first commit

---
+++ src/components/Item.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card my-2 item-todo">
+        <p>{{task}}</p>
+        <div class="btn-supr btn btn-danger">X</div>
+    </div>
+</template>
+
+
+
+
+<script>
+    export default {
+        name: 'Item',
+        props: ['task', 'supp']
+    }
+</script>
+
+
+
+<style>
+    .item-todo{
+        position:relative;
+        padding: 20px 60px 20px 30px;
+        font-size: 20px;
+        height: 80px;
+    }
+    .btn-supr{
+        position: absolute;
+        top: 10px;
+        right: 10px;
+    }
+    .item-todo p{
+    margin-bottom: 0!important;
+    }
+</style>
---
+++ src/components/MainForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h1>New Task</h1>
+        <form>
+            <div class="form-group">
+                <label for="task">Task</label>
+                <input v-model="task" type="text" class="form-control" id="task">
+                <button v-on:click="submitTask" class="btn btn-primary"> Add task</button>
+                <p>{{task}}</p>
+            </div>
+        </form>
+
+
+        <h1>Current Tasks</h1>
+        <ul>
+            <li v-bind:key="index" v-for="(task, index) in tasks">
+                <item v-bind:task="task"></item>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+
+
+<script>
+import Item from './Item'
+export default {
+    name: 'MainForm',
+    data() {
+        return{
+            formData: {
+                task: ''
+            },
+            tasks: ['python', 'react', 'java', 'php']
+            
+        }
+    },
+    methods: {
+        submitTask: function(){
+            this.tasks.append(this.task)
+        }
+    },
+    components: {
+        'item': Item
+    }
+    
+}
+</script>
+
+
+
+<style>
+
+</style>
